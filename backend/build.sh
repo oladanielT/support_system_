8b800f75e126d4ebc35e71fa9475e040689cf7a8
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2025-08-22 16:00:42 +0100

    updated

--- backend/build.sh
+++ backend/build.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
-set -e
+# #!/bin/bash
+# set -e
 
-# Upgrade pip and build tools
-python -m pip install --upgrade pip setuptools wheel
+# # Upgrade pip and build tools
+# python -m pip install --upgrade pip setuptools wheel
 
-# Install dependencies
-pip install -r requirements.txt
+# # Install dependencies
+# pip install -r requirements.txt
 
-# Run migrations
-python manage.py migrate
+# # Run migrations
+# python manage.py migrate
 
-# Collect static files
-python manage.py collectstatic --noinput
+# # Collect static files
+# python manage.py collectstatic --noinput
